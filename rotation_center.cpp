5b2a9b7db9931e090301674e903974d984d5b2ae
Timur A. Fatkhullin <amirych@gmail.com>
2015-07-14 18:31:40 +0300

    add AsciiFile class

--- rotation_center.cpp
+++ rotation_center.cpp
@@ -1,12 +1,16 @@
 #include<iostream>
 #include<fstream>
-#include<libgen.h>
+//#include<libgen.h>
 //#include<cstdlib>
 #include<cstdio>
 #include<list>
 
+#define BOOST_NO_CXX11_SCOPED_ENUMS // special definition to fix Boost's copy_file and -std=c++11 linking error
 #include<boost/program_options.hpp>
 #include<boost/filesystem.hpp>
+
+
+#include"ascii_file.h"
 
 using namespace std;
 
@@ -23,13 +27,15 @@ namespace po = boost::program_options;
 #define ROTCEN_ERROR_NOT_ENOUGH_FILES 70
 #define ROTCEN_ERROR_APP_FAILED 80
 #define ROTCEN_ERROR_CANNOT_CREATE_FILE 90
+#define ROTCEN_ERROR_BAD_DATA 100
 
 
-static string ROTCEN_SEX_PARAM_FILE="sex.param";
+static string ROTCEN_SEX_PARAM_FILE = "sex.param";
+static string ROTCEN_MATCH_REF_CAT = "ref.cat";
 
 static string ROTCEN_SEX_EXE = "sex";
 static string ROTCEN_AST_EXE = "solve-field";
-static string ROTCEN_MATCH_EXE = "sex";
+static string ROTCEN_MATCH_EXE = "match";
 
 
 static string join_vector(vector<string> &vec)
@@ -39,21 +45,71 @@ static string join_vector(vector<string> &vec)
     return str;
 }
 
+
+static int run_external(string &cmd_str)
+{
+    int ret = system(cmd_str.c_str()); // try to run external command
+
+    int exit_code = WEXITSTATUS(ret);
+
+    if ( ret == -1 ) {
+        return ret;
+    }
+    if ( exit_code != 0 ) {
+        return exit_code;
+    }
+
+    return 0;
+}
+
+
+static int read_catalog(string &filename, size_t N_items, vector<vector<double> > &data)
+{
+    AsciiFile cat(filename.c_str());
+
+    if ( !cat.good() ) return ROTCEN_ERROR_INVALID_FILENAME;
+
+    data.clear();
+
+    data = vector<vector<double> >(N_items);
+
+    vector<double> vec;
+    AsciiFile::AsciiFileFlag line_flag;
+
+
+    while ( (line_flag = cat.ReadLine(N_items,&vec)) != AsciiFile::Eof ) {
+        if ( line_flag == AsciiFile::InvalidData ) {
+            cat.close();
+            return ROTCEN_ERROR_BAD_DATA;
+        }
+        if ( line_flag == AsciiFile::DataString ) {
+            for ( size_t i = 0; i < N_items; ++i ) data[i].push_back(vec[i]);
+        }
+    }
+
+    cat.close();
+    return ROTCEN_ERROR_OK;
+}
+
+
 int main(int argc, char* argv[])
 {
 
 
     vector<float> sex_thresh(1,5.0); // sextractor's THRESH default value
-    vector<float> match_tol(1,0.5); // default radius of coordinate matching. It is in arcsecs for astrometrical solution or
+    vector<float> match_tol;        // default radius of coordinate matching. It is in arcsecs for astrometrical solution or
                                     // dimensionless value for 'match application' solution ('matchrad' parameter)
+                                    // The actuall default value is set below according to '--use-match'
 
     vector<string> sex_pars = {"-DETECT_TYPE CCD","-SATUR_LEVEL 40000","-CHECKIMAGE_TYPE NONE", "-FILTER N"};
 
     vector<string> solve_field_pars = {"--no-plots"};
 
-    vector<string> match_pars = {""};
+    vector<string> match_pars = {"id1=0 id2=0 min_scale=0.9 max_scale=1.1 linear"}; // default 'match' commandline parameters
 
     vector<string> sex_cat_prefix = {"obj_"};
+
+    vector<string> ast_prefix = {"wcs_"}; // astrometry-calibrated file prefix
 
     string input_list_filename;
 
@@ -167,6 +223,10 @@ int main(int argc, char* argv[])
 
         use_match = true;
 
+        if ( !vm.count("radius") ) { // use default value
+            match_tol = {4.0};
+        }
+
         sex_pars.push_back("-GAIN 1.0");
         sex_pars.push_back("-PARAMETERS_NAME " + ROTCEN_SEX_PARAM_FILE);
         sex_pars.push_back("-CATALOG_TYPE ASCII");
@@ -204,6 +264,10 @@ int main(int argc, char* argv[])
         }
         use_sex = true;
         solve_field_pars.push_back("--use-sextractor");
+
+        if ( !vm.count("radius") ) { // use default value
+            match_tol = {0.5};
+        }
     }
 
 
@@ -221,9 +285,8 @@ int main(int argc, char* argv[])
         }
 
         // check input files
-        while ( input_list_file.good() ) {
-            input_list_file >> str;
-//            if ( !ifstream(str.c_str()).good() ) {
+
+        while ( (input_list_file >> str).good() ) {
             if ( !boost::filesystem::exists(str) ) {
                 cerr << "Cannot find " << str.c_str() << " input file!\n";
                 throw (int)ROTCEN_ERROR_INVALID_FILENAME;
@@ -240,25 +303,32 @@ int main(int argc, char* argv[])
 
         // run object detection and astrometry
 
+        string cmd_pars = join_vector(sex_pars);
+
+        cout << "\nObjects detection:\n";
+
         for ( auto it_file = input_files.begin(); it_file != input_files.end(); ++it_file ) {
             if ( use_match ) { // skip astrometry, just detect objects using sextractor
                 boost::filesystem::path pp = *it_file;
                 string path = pp.parent_path().string();
                 string file = boost::filesystem::basename(*it_file);
 
-                file = path + sex_cat_prefix.back() + file + ".cat";
-                sex_pars.push_back("-CATALOG_NAME " + file);
-                string cmd_str = ROTCEN_SEX_EXE + " " + join_vector(sex_pars) + " " + *it_file + " >/dev/null 2>&1";
+                file = path + boost::filesystem::path::preferred_separator + sex_cat_prefix.back() + file + ".cat";
+                string cmd_str = ROTCEN_SEX_EXE + " " + cmd_pars + " -CATALOG_NAME " +
+                                 file + " " + *it_file + " >/dev/null 2>&1";
 
-                int ret = system(cmd_str.c_str()); // try to run command 'sex' (Bertin's sextractor)
-                int exit_code = WEXITSTATUS(ret);
-                if ( ret == -1 || exit_code != 0 ) {
+                cout << "  Run SExtractor for " + *it_file + " ... ";
+
+                int ret = run_external(cmd_str); // try to run command 'sex' (Bertin's sextractor)
+                if ( ret ) {
+                    cout << "Failed!\n";
                     cerr << "Something wrong while run application 'sex'!\n";
                     throw (int)ROTCEN_ERROR_APP_FAILED;
                 }
 
+                cout << "OK!\n";
+
                 sex_cats.push_back(file);
-                sex_pars.pop_back(); // delete "-CATALOG_NAME" parameter from commandline list
             } else { // perform astrometry
 
             }
@@ -266,9 +336,83 @@ int main(int argc, char* argv[])
 
         // matching objects
 
-        if ( use_match ) {
+        vector<vector<double> > obj_cat(3*input_files.size()); // NUMBER, X_IMAGE and Y_IMAGE columns
+        vector<vector<double> > obj_id(input_files.size());
+        vector<vector<double> > current_cat;
 
-        } else {
+
+        if ( use_match ) { // use of 'match' application
+            cout << "\nMatching objects (use of 'match' application):\n";
+
+            auto it_file = sex_cats.begin();
+            ++it_file; // point to the second catalog
+
+
+            boost::filesystem::copy_file(sex_cats.front(),ROTCEN_MATCH_REF_CAT,boost::filesystem::copy_option::overwrite_if_exists);
+
+            // read the first catalog
+            int ret = read_catalog(sex_cats.front(), 3, current_cat);
+            if ( ret != ROTCEN_ERROR_OK ) {
+                cerr << "Something wrong while reading " << sex_cats.front() << " file!\n";
+                throw ret;
+            }
+            obj_cat[0] = current_cat[0]; // NUMBER
+            obj_cat[1] = current_cat[1]; // X_IMAGE
+            obj_cat[2] = current_cat[2]; // Y_IMAGE
+
+            string cmd_str;
+            string pp = join_vector(match_pars);
+
+            string matchedA = "matched.mtA";
+            string matchedB = "matched.mtB";
+
+            for ( long i_cat = 1; it_file != sex_cats.end(); ++it_file, ++i_cat ) {
+
+                // read current catalog
+
+                ret = read_catalog(*it_file, 3, current_cat);
+                if ( ret != ROTCEN_ERROR_OK ) {
+                    cerr << "Something wrong while reading " << *it_file << " file!\n";
+                    throw ret;
+                }
+                obj_cat[i_cat*3] = current_cat[0]; // NUMBER
+                obj_cat[i_cat*3+1] = current_cat[1]; // X_IMAGE
+                obj_cat[i_cat*3+2] = current_cat[2]; // Y_IMAGE
+
+                cmd_str = ROTCEN_MATCH_EXE + " " + ROTCEN_MATCH_REF_CAT + " 1 2 3 " + *it_file + " 1 2 3 " +
+                          pp + " matchrad=" + to_string(match_tol.back()) + " >/dev/null 2>&1";
+
+                cout << "  Run match for " + *it_file + " ... ";
+
+                int ret = run_external(cmd_str);
+                if ( ret ) {
+                    cout << "Failed!\n";
+                    cerr << "Something wrong while run application 'match'!\n";
+                    throw (int)ROTCEN_ERROR_APP_FAILED;
+                }
+
+                cout << "OK!\n";
+
+                // read result matched catalogs (only ID(NUMBER) columns)
+
+                ret = read_catalog(matchedA, 1, current_cat);
+                if ( ret != ROTCEN_ERROR_OK ) {
+                    cerr << "Something wrong while reading matched.mtA file!\n";
+                    throw ret;
+                }
+                // REARRANGE HERE!!!
+                obj_id[0] = current_cat[0];
+
+                ret = read_catalog(matchedB, 1, current_cat);
+                if ( ret != ROTCEN_ERROR_OK ) {
+                    cerr << "Something wrong while reading matched.mtB file!\n";
+                    throw ret;
+                }
+                obj_id[i_cat] = current_cat[0];
+
+            }
+
+        } else { // use of astrometrical solution
 
         }
 
@@ -283,6 +427,13 @@ int main(int argc, char* argv[])
         return err;
     }
 
+    // delete temporary files
+    if ( use_match && !dont_delete ) {
+        boost::filesystem::remove(ROTCEN_SEX_PARAM_FILE);
+        for (auto filename = sex_cats.begin(); filename != sex_cats.end(); ++filename) {
+            boost::filesystem::remove(*filename);
+        }
+    }
 
     return ROTCEN_ERROR_OK;
 }
